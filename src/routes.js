4f0e040a301aaafabffe3630f9a046fa3e533a39
Chanikya <chanikyaprakash@gmail.com>
2019-11-22 20:30:00 +0530

    Initial templates and routing implementation

---
+++ src/routes.js
@@ -0,0 +1,32 @@
+(function(){
+	'use strict';
+
+	// angular.module('RestaurantApp',['ui.router']);
+
+	angular.module('RestaurantApp')
+	.config(RoutesConfig);
+
+	RoutesConfig.$inject = ['$stateProvider','$urlRouterProvider'];
+	function RoutesConfig($stateProvider,$urlRouterProvider){
+		// Redirect to home page if no other URL matches
+		$urlRouterProvider.otherwise('/');
+
+		// *** Set up UI states ***
+		$stateProvider
+
+		.state('/',{
+			url: '/',
+			templateUrl:'src/restaurantMenu/templates/home.template.html'
+		})
+		.state('cat',{
+			url: '/cat',
+			templateUrl:'src/restaurantMenu/templates/categories.template.html'
+		})
+		.state('items',{
+			url: '/items',
+			templateUrl:'src/restaurantMenu/templates/itemsList.template.html'
+		});
+
+	}
+
+})();
